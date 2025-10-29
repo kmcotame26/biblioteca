b2c189e4b259798b8bfe1b6dd28380ceddbc4d2e
kmcotame26 <kmcotame26@ucatolica.edu.co>
2025-10-28 20:51:29 -0500

    Biblioteca en reparacion

--- modelos/mod_libro.py
+++ modelos/mod_libro.py
@@ -9,10 +9,11 @@ class Libro(SQLModel, table=True):
     isbn: str
     ano_publicacion: int
     copias_disponibles: int = 1
+    autor_id: Optional[int] = Field(default=None, foreign_key="autor.id")
     editorial_id: Optional[int] = Field(default=None, foreign_key="editorial.id")
     genero_id: Optional[int] = Field(default=None, foreign_key="genero.id")
 
-    autor: Optional["Autor"] = Relationship("Autor",back_populates="libros")
+    autor: Optional["Autor"] = Relationship(back_populates="libros")
     editorial: Optional["Editorial"] = Relationship(back_populates="libros")
     genero: Optional["Genero"] = Relationship(back_populates="libros")
     prestamos: List["Prestamo"] = Relationship(back_populates="libro")
