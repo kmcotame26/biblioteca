05095ff1a220beec8095c66295ff95c6b2303ca9
kmcotame26 <kmcotame26@ucatolica.edu.co>
2025-10-28 20:29:49 -0500

    esperando

--- modelos/mod_libro.py
+++ modelos/mod_libro.py
@@ -12,7 +12,7 @@ class Libro(SQLModel, table=True):
     editorial_id: Optional[int] = Field(default=None, foreign_key="editorial.id")
     genero_id: Optional[int] = Field(default=None, foreign_key="genero.id")
 
-    autor = Relationship("Autor",back_populates="libros")
-    editorial = Relationship("Editorial",back_populates="libros")
-    genero = Relationship("Genero", back_populates="libros")
+    autor: Optional["Autor"] = Relationship("Autor",back_populates="libros")
+    editorial: Optional["Editorial"] = Relationship(back_populates="libros")
+    genero: Optional["Genero"] = Relationship(back_populates="libros")
     prestamos: List["Prestamo"] = Relationship(back_populates="libro")
